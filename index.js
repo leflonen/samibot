bd1d2c0e316f5b86caf2789f504f1db8aefa4926
Juho Lehtonen <lehtonen.juho@hotmail.fi>
2017-12-12 17:56:18 +0200

    Vitsi vai sää napit lisätty sekä vitsun haku

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 
 module.exports = function(bp) {
 
-  bp.hear(/GET_STARTED/i, (event, next) => {
-    event.reply('#start')
+  bp.hear(/GET_STARTED|mo|moro|moron|moi|hei/i, (event, next) => {
+    event.reply('#startConvo')
   })
 
-  bp.hear(/mo|moro|moron|moi|hei/i, (event, next) => {
-    event.reply('#welhei')
+  bp.hear('STARTCONVO.B1', (event, next) => {
+    let http = require('http');
+
+    let options = {
+      host: 'api.icndb.com',
+      path:'/jokes/random',
+      method: 'GET',
+      header: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    };
+    let toparse = '';
+    callback = function(response) {
+        response.on('data', function (chunk) {
+          toparse += chunk
+        });
+        console.log(toparse);
+        response.on('end', function () {
+          let json = JSON.parse(toparse);
+          joke = json.value.joke;
+          event.reply('#jokeResponse', { joke })
+        })
+    };
+    http.request(options, callback).end();
+  })
+
+
+  bp.hear('STARTCONVO.B2', (event, next) => {
+    event.reply('#getLocation')
   })
 
   bp.hear(/hej/i, (event, next) => {
     event.reply('#welhej')
   })
 
-  bp.hear({ type: 'location' }, (event, next) => {
-    var http = require('http');
 
-    var opt = {
+
+  bp.hear({ type: 'location' }, (event, next) => {
+    let http = require('http');
+
+    let opt = {
       host: 'api.openweathermap.org',
       pre: "/data/2.5/weather",
       lat: "?lat=" + event.raw.payload.coordinates.lat,
@@ -25,20 +52,20 @@ module.exports = function(bp) {
       lang: "&lang=fi",
       unit: "&units=metric"
     };
-    var options = {
+    let options = {
       host: opt.host,
       path: opt.pre + opt.lat + opt.lon + opt.lang + opt.unit + opt.app,
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
     };
-    var toparse = '';
+    let toparse = '';
     callback = function(response) {
         response.on('data', function (chunk) {
           toparse += chunk
         });
         response.on('end', function () {
-          var json = JSON.parse(toparse);
-          var obj = {
+          let json = JSON.parse(toparse);
+          let obj = {
           	name: json.name,
           	des: json.weather[0].description,
           	temp: json.main.temp,
@@ -58,6 +85,5 @@ module.exports = function(bp) {
   }, (event, next) => {
     event.reply('#wat')
   })
-  
-};
 
+};
